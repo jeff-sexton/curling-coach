878e153e4e937737247af1d4fd97e83b546ce408
Alan Marx <alanmarx@Valerias-MBP.hitronhub.home>
2020-05-09 14:15:18 -0700

    added validations for the game and end models

--- app/models/end.rb
+++ app/models/end.rb
@@ -1,4 +1,8 @@
 class End < ApplicationRecord
   has_many :shots
   belongs_to :game
+  validates :score_team1, :score_team2, numericality: { only_integer: true }
+  validates :game, presence: true
 end
+
+
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   has_many :game_participations
   has_many :ends
+  validates :date_time, :location, presence: true
+  validates :completed, inclusion: { in: [true, false], message: "value must be either true or false" }          
 end
