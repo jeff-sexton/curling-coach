f7f36de9574a377ad69bd14031782df697568b10
Alan Marx <alanmarx@Valerias-MBP.hitronhub.home>
2020-05-09 17:15:12 -0700

    added validations for the end, game participation, player, shot and team models

--- app/models/end.rb
+++ app/models/end.rb
@@ -1,8 +1,8 @@
 class End < ApplicationRecord
   has_many :shots
   belongs_to :game
-  validates :score_team1, :score_team2, numericality: { only_integer: true }
   validates :game, presence: true
+  validates :score_team1, :score_team2, numericality: { only_integer: true }, allow_nil: true
 end
 
 
--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,4 +1,31 @@
 class Shot < ApplicationRecord
   belongs_to :player
   belongs_to :end
+  validates :player, :end, :rock_paths, presence: true
+  validates :shot_type, inclusion:   { in: %w(Draw Front Guard Raise Wick Freeze TakeOut HitAndRoll 
+                                              Clearing DoubleTakeOut PromotionTakeOut NotScored) }
+  validates :shot_number, inclusion: { in: 1..16,
+                                       message: "must be a number between 1 and 16" },
+                                       allow_nil: true
+  validates :rotation, inclusion:    { in: %w(clockwise counterclockwise), 
+                                       message: "must be either clockwise or counterclockwise" },
+                                       allow_nil: true
+  validates :rating, inclusion:      { in: 1..4, 
+                                       message: "must be a number between 1 and 4" }, 
+                                       allow_nil: true
+  validates :sweep_score, inclusion   { in: 1..10,
+                                       message: "must be a number bewtween 1 and 10" },
+                                       allow_nil: true
+                                      
+                      
+  
+
+  # def not_scored_type?
+  #   self.shot_type == 'NotScored'
+  # end
 end
+
+
+# t.text "comments"
+# t.json "target_position"
+# t.time "hog_time"
