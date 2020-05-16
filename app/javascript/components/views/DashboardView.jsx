db95aab8ba70c0ee783c1a911bca93d183c6c975
yasu71 <yasuyo71@gmail.com>
2020-05-15 23:48:20 -0700

    Added a loading icon with animation

--- app/javascript/components/views/DashboardView.jsx
+++ app/javascript/components/views/DashboardView.jsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Dashboard from '../Dashboard';
 import axios from 'axios';
 
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+import LoadingIcon from '../Assets/icon.svg';
+
 const DashboardView = ({ handleGameSelection }) => {
   const [gameList, setGameList] = useState([]);
   const [dashboardLoaded, setDashboardLoaded] = useState(false);
@@ -13,6 +19,26 @@ const DashboardView = ({ handleGameSelection }) => {
     });
   }, []);
 
+  const useStyles = makeStyles({
+    card: {
+      maxWidth: '20%',
+    },
+    test: {
+      width: 250,
+      position: 'relative',
+      animation: '$animation 5s infinite',
+    },
+    root: {
+      width: 150,
+    },
+    '@keyframes animation': {
+      from: { left: -10 },
+      to: { left: '70%' },
+    },
+  });
+  
+  const classes = useStyles();
+  
   return (
     <div>
       {dashboardLoaded && (
@@ -23,9 +49,23 @@ const DashboardView = ({ handleGameSelection }) => {
       )}
 
       {!dashboardLoaded && (
-        <div>
-          <h1>LOADING</h1>
-        </div>
+        <Box
+          className={classes.test}
+          display="flex"
+          justifyContent="start"
+          alignItems="center"
+        >
+          <Box>
+            <h2>LOADING</h2>
+          </Box>
+          <Box>
+            <CardMedia
+              image={LoadingIcon}
+              component="img"
+              classes={{ root: classes.root }}
+            />
+          </Box>
+        </Box>
       )}
     </div>
   );
