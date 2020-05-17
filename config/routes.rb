7333a3d8cc7dea8845a9b857b9d6db2fecf4f08c
jeff-sexton <sexton.jeffp@gmail.com>
2020-05-16 17:17:41 -0700

    Adding route to access stats controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :games, only: [:index, :show, :create, :update]
     resources :ends, only: [:index, :create, :update]
     resources :shots, except: [:new, :show]
+    resources :stats, only: [:show]
 
   end
 end
