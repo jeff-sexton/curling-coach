700b33b901ec847dbe8152066a28e523ec28d984
jeff-sexton <sexton.jeffp@gmail.com>
2020-06-03 10:57:09 -0700

    Adding more tests

--- spec/requests/api:games_request_spec.rb
+++ spec/requests/api:games_request_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe 'Api:games', type: :request do
   # Setup
   before :each do
     @game1 = Game.create!({
+                            id: 1,
                             date_time: 3.days.ago,
-                            location: 'Vancouver',
+                            location: 'Game1Location',
                             completed: false
                           })
-    @team1 = Team.find_or_create_by!({ id: 1, team_name: "Jeff's Team" })
-    @team2 = Team.find_or_create_by!({ id: 2, team_name: "Yasu's Team" })
+    @team1 = Team.find_or_create_by!({ id: 1, team_name: 'Team1' })
+    @team2 = Team.find_or_create_by!({ id: 2, team_name: 'Team2' })
+    @team3 = Team.find_or_create_by!({ id: 3, team_name: 'Team3' })
+    @team4 = Team.find_or_create_by!({ id: 4, team_name: 'Team4' })
     @game1.game_participations.create!({
                                          team_id: 1
                                        })
@@ -19,22 +22,55 @@ RSpec.describe 'Api:games', type: :request do
     @game1.game_participations.create!({
                                          team_id: 2
                                        })
+
+    @game2 = Game.create!({
+                            id: 2,
+                            date_time: 3.days.ago,
+                            location: 'Game2Location',
+                            completed: false
+                          })
+    @game2.game_participations.create!({
+                                         team_id: 3
+                                       })
+
+    @game2.game_participations.create!({
+                                         team_id: 4
+                                       })
   end
   describe '.index' do
-    it 'responds with a valid json' do
+    before :each do
       headers = { 'ACCEPT' => 'application/json' }
       get '/api/games', params: {}, headers: headers
+    end
 
+    it 'responds with a valid json' do
       expect(response.content_type).to eq('application/json; charset=utf-8')
       expect(response).to have_http_status(:ok)
     end
 
-    it 'response contains the correct data' do
+    it 'response contains all defined games and associated teams' do
+      expect(response.body).to include('Game1Location')
+      expect(response.body).to include('Game2Location')
+      expect(response.body).to include('Team1')
+      expect(response.body).to include('Team2')
+      expect(response.body).to include('Team3')
+      expect(response.body).to include('Team4')
+    end
+
+    it 'response is an array of hashes' do
+      data = JSON.parse(response.body)
+      expect(data).to be_an_instance_of(Array)
+      expect(data[0]).to be_an_instance_of(Hash)
+    end
+  end
+
+  describe '.show' do
+    it 'responds with a valid json' do
       headers = { 'ACCEPT' => 'application/json' }
-      get '/api/games', params: {}, headers: headers
-      
-      expect(response.body).to include('Vancouver')
-      expect(response.body).to include("Jeff's Team")
+      get "/api/games/#{@game1[:id]}", params: {}, headers: headers
+
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+      expect(response).to have_http_status(:ok)
     end
   end
 end
