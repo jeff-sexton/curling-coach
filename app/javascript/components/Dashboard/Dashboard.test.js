581fc7c1d320baff542ce48fa27fc014662efc67
jeff-sexton <sexton.jeffp@gmail.com>
2020-06-08 13:55:06 -0700

    add more test for dashboard component

--- app/javascript/components/Dashboard/Dashboard.test.js
+++ app/javascript/components/Dashboard/Dashboard.test.js
@@ -1,37 +1,54 @@
-import React from "react";
+/* global expect */
+/* global jest */
 
-import {
-  render,
-  cleanup,
-  waitForElement,
-  fireEvent,
-  getByText,
-  getByAltText,
-  getByPlaceholderText,
-  prettyDOM,
-  getAllByTestId,
-} from "@testing-library/react";
+import React from 'react';
 
-import Dashboard from "./Dashboard";
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
 
 import gameData from '../../__mocks__/dashboardViewSeed';
 
-
-
 afterEach(cleanup);
 
-describe("Dashboard", () => {
-  it("Loads a list of games", async () => {
-    const { getByText } = render(<Dashboard gameList={gameData}/>);
-    // await waitForElement(() => getByText("Dashboard"));
+describe('Dashboard', () => {
+  it('Loads games with the correct data displayed', () => {
+    const { getByText } = render(<Dashboard gameList={gameData} />);
 
-    // console.log(getByText(/Calgary/i));
-    expect(getByText(/Calgary/i)).toBeInTheDocument();
+    expect(getByText(gameData[0].location)).toBeInTheDocument();
+  });
+  it('Loads the correct number of games from the sample data', () => {
+    const { getAllByTestId } = render(<Dashboard gameList={gameData} />);
+
+    expect(getAllByTestId('game-preview')).toHaveLength(gameData.length);
   });
 
+  it('Sets the game id of a clicked preview', () => {
+    const handleGameSelection = jest.fn();
+    const { getByText } = render(
+      <Dashboard
+        gameList={gameData}
+        handleGameSelection={handleGameSelection}
+      />
+    );
 
+    fireEvent.click(getByText(gameData[0].location));
 
+    expect(handleGameSelection).toHaveBeenCalledTimes(1);
+    expect(handleGameSelection).toHaveBeenCalledWith(gameData[0].id);
+  });
+  it('Sets the game id of a clicked stats button', () => {
+    const handleStatsSelection = jest.fn();
+    const { getAllByText } = render(
+      <Dashboard
+        gameList={gameData}
+        handleStatsSelection={handleStatsSelection}
+      />
+    );
 
+    fireEvent.click(getAllByText('Stats')[1]);
 
-
+    expect(handleStatsSelection).toHaveBeenCalledTimes(1);
+    expect(handleStatsSelection).toHaveBeenCalledWith(gameData[1].id);
+  });
 });
