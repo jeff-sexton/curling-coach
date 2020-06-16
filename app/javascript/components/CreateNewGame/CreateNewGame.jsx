5f4608dc13de044f254def515f0445c20c821f9a
jeff-sexton <sexton.jeffp@gmail.com>
2020-06-16 11:07:49 -0700

    Add CreateNewGame to modal from menu

--- app/javascript/components/CreateNewGame/CreateNewGame.jsx
+++ app/javascript/components/CreateNewGame/CreateNewGame.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
+
 import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
 import DateTime from './DateTime';
 import Location from './Location';
 import CreateNewGameStyles from './CreateNewGameStyles';
 
-const useStyles = makeStyles(CreateNewGameStyles, (theme) => ({
+const useStyles = makeStyles( (theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 'auto',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
   root: {
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
     },
   },
+
 }));
 
-const CreateNewGame = () => {
+const CreateNewGame = ({ open, handleClose }) => {
   const classes = useStyles();
 
-  return (
-    <div>
+  const body = (
+    <div datatag={'here'} className={classes.paper}>
       <form className={classes.root} noValidate autoComplete="off">
         <Box pl={3} pr={3}>
           <DateTime />
@@ -27,6 +41,17 @@ const CreateNewGame = () => {
         </Box>
       </form>
     </div>
+  );
+
+  return (
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="new-game"
+        aria-describedby="new-game-description"
+      >
+        {body}
+      </Modal>
   );
 };
 
